1c1fa464fa24a134024890f12ab2eec828fa6114
Roman Wang <roman@Romans-MacBook-Pro.local>
2020-05-07 17:12:54 -0400

    Organize imports. Write keygen. Organize helper methods.

--- ntru.py
+++ ntru.py
@@ -1,9 +1,13 @@
 import math
+import random
+import itertools
 import numpy as np
+from numpy.fft import rfft, irfft, multiply, prod
 from sympy import GF, invert
 from sympy.abc import x
 from sympy import ZZ, QQ, FF, Poly
-import random
+from sympy import intt, ntt, fft, ifft, simplify
+from sympy.polys import polytools
 
 def is_prime(n):
     for i in range(2, int(n ** 0.5) + 1):
@@ -11,27 +15,58 @@ def is_prime(n):
             return False
     return True
 
-def is_power_of_two(n):
-    return n != 0 and (n & (n - 1) == 0)
-
 def gcd(p, q):
     if p is 0:
         return q
     return gcd(q%p,p)
 
-### Parameter generation
-# Generate f, g
-# Generate N, p, q
-# Check invertibility of f mod p, q
-# Create h from p, f, g
+def find_inverse(p, m):
+    if p % m == 1:
+        return 1
+    for candidate in range(2, m):
+        # print((p*candidate) % m)
+        if (p*candidate) % m == 1:
+            return candidate
+    raise Exception("no inverse found")
+    print("No inverse found")
+    return False
 
-def generate_polynomial(N):
-    coefficients = random.choices([-1, 0, 1], k=N-1)
-    return Poly(coefficients, x).set_domain(ZZ)
+def get_coprime(p_range, q_range):
+    for p_candidate in p_range:
+        for q_candidate in q_range:
+            if gcd(p_candidate, q_candidate) == 1:
+                yield p_candidate, q_candidate
+
+MOD = Poly(x**N-1,x).set_domain(ZZ)
+
+def apply_dual_modulus(expression, q, N):
+    return expression.trunc(q) % MOD
+
+def generate_polynomial(N, d=13, neg_ones_diff=1):
+
+    def random_poly(length, d, neg_ones_diff):
+        return Poly(np.random.permutation(
+            np.concatenate((np.zeros(length - 2 * d - neg_ones_diff), np.ones(d), -np.ones(d + neg_ones_diff)))),
+            x).set_domain(ZZ)
+    yield random_poly(N, d, neg_ones_diff)
 
 def invert_polynomial(f_polynomial, N, p):
-    modulus_polynomial=Poly(x**N-1,x).set_domain(ZZ)
+    modulus_polynomial=MOD
     return invert(f_polynomial, modulus_polynomial, domain=GF(p))
+
+def invert_poly(f_poly, N, p):
+    R_poly = MOD
+    inv_poly = None
+    if is_prime(p):
+        log.debug("Inverting as p={} is prime".format(p))
+        inv_poly = invert(f_poly, R_poly, domain=GF(p))
+    elif is_2_power(p):
+        log.debug("Inverting as p={} is 2 power".format(p))
+        inv_poly = invert(f_poly, R_poly, domain=GF(2))
+        e = int(math.log(p, 2))
+        for i in range(1, e):
+            log.debug("Inversion({}): {}".format(i, inv_poly))
+            inv_poly = ((2 * inv_poly - f_poly * inv_poly ** 2) % R_poly).trunc(p)
 
 def generate_parameters(N_range=range(250, 2500), p_range=range(250, 2500), q_range=range(3, 4)):
     # Generate p, q, N such that
@@ -41,7 +76,7 @@ def generate_parameters(N_range=range(250, 2500), p_range=range(250, 2500), q_ra
 
     # Todo: enforce N > q >> p
 
-    ATTEMPTS = 1000
+    ATTEMPTS = 100
 
     def sample_range(number_range, looking_for_prime=True):
         if looking_for_prime:
@@ -53,24 +88,20 @@ def generate_parameters(N_range=range(250, 2500), p_range=range(250, 2500), q_ra
             return random.choice(number_range)
 
     def get_coprime(p_range, q_range):
-        p = None
-        q = None
         for p_candidate in p_range:
-            if is_prime(p_candidate):
-                p = p_candidate
-                break
-        for q_candidate in q_range:
-            if is_prime(q_candidate):
-                q = q_candidate
-                break
-        if p is not None and q is not None:
-            return p, q
-        raise Exception("Cannot find coprime p, q")
+            for q_candidate in q_range:
+                if gcd(p_candidate, q_candidate) == 1:
+                    yield p_candidate, q_candidate
 
     def get_f_g_h(p, q, N):
-        for iterations in range(ATTEMPTS):
-            f = generate_polynomial(N)
-
+        current_attempt=0
+        for f in generate_polynomial(N):
+            current_attempt +=1
+            if current_attempt>=ATTEMPTS:
+                break
+            #f = next(generate_polynomial(N))
+            print("Candidate")
+            print(f)
             try:
                 f_p = invert_polynomial(f, N, p)
             except:
@@ -79,15 +110,18 @@ def generate_parameters(N_range=range(250, 2500), p_range=range(250, 2500), q_ra
                 f_q = invert_polynomial(f, N, q)
             except:
                 continue
-            g = generate_polynomial(N)
+            g = next(generate_polynomial(N))
             h = (p*f_q*g).trunc(q)
             return f, f_p, f_q, g, h
         raise Exception("Cannot find invertible f")
 
     N = sample_range(N_range, looking_for_prime=True)
-    p, q = get_coprime(p_range, q_range)
-    f, f_p, f_q, g, h = get_f_g_h(p, q, N)
-    return p, q, f, f_p, f_q, g, h
-
-def apply_dual_modulus(expression, q, N):
-    return expression.trunc(q) % N
+    f = None
+    for p, q in get_coprime(p_range, q_range):
+        try:
+            f, f_p, f_q, g, h = get_f_g_h(p, q, N)
+            if f is not None:
+                return p, q, f, f_p, f_q, g, h
+        except:
+            pass
+    raise Exception("invertible f not found")
